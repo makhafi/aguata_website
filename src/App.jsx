3e4cdce3b010dbbd3d9bb951f7714ddf3ef5f4fa
makhafi <150081358+makhafi@users.noreply.github.com>
2023-11-07 04:01:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState,useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 import './App.css'
 import { BrowserRouter as Router,Switch,Route } from "react-router-dom"
 import Navbar from './navbar'
+import ReactDOM from "react-dom
  
 
 
